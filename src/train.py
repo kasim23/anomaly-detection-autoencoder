aede5dc8783ba1ce0885e394dc102f2723163e76
kasim23 <qsudais@gmail.com>
2025-03-13 17:34:50 -0400

    training module

---
+++ src/train.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+import copy
+import numpy as np
+
+
+def train_model(model, train_dataset, val_dataset, n_epochs):
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+  criterion = nn.L1Loss(reduction='sum').to(device)
+  history = dict(train=[], val=[])
+
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_loss = 10000.0
+  
+  for epoch in range(1, n_epochs + 1):
+    model = model.train()
+
+    train_losses = []
+    for seq_true in train_dataset:
+      optimizer.zero_grad()
+
+      seq_true = seq_true.to(device)
+      seq_pred = model(seq_true)
+
+      loss = criterion(seq_pred, seq_true)
+
+      loss.backward()
+      optimizer.step()
+
+      train_losses.append(loss.item())
+
+    val_losses = []
+    model = model.eval()
+    with torch.no_grad():
+      for seq_true in val_dataset:
+
+        seq_true = seq_true.to(device)
+        seq_pred = model(seq_true)
+
+        loss = criterion(seq_pred, seq_true)
+        val_losses.append(loss.item())
+
+    train_loss = np.mean(train_losses)
+    val_loss = np.mean(val_losses)
+
+    history['train'].append(train_loss)
+    history['val'].append(val_loss)
+
+    if val_loss < best_loss:
+      best_loss = val_loss
+      best_model_wts = copy.deepcopy(model.state_dict())
+
+    print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
+
+  model.load_state_dict(best_model_wts)
+  return model.eval(), history
