9bb8bb4133c492464ab9813e88e46a7709bbfa14
kasim23 <qsudais@gmail.com>
2025-03-13 18:00:53 -0400

    create datasets

---
+++ utils/datasets.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+
+#Convert our examples into tensors, so we can use them to train the Autoencoder
+
+def create_dataset(df):
+  sequences = df.astype(np.float32).to_numpy().tolist()
+  dataset = [torch.tensor(s).unsqueeze(1).float() for s in sequences]
+  n_seq, seq_len, n_features = torch.stack(dataset).shape
+  return dataset, seq_len, n_features
